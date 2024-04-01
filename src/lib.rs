2af8d6dd5d8d21172079ab977cc0a6f61792499c
NoodleSamaChan <lferraraccio@outlook.com>
2024-04-01 23:32:14 +0200

    added main

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+use clap::{Parser, ValueEnum};
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use rand::Rng;
+use std::fmt;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::time::{Duration, Instant};
+use window_rs::WindowBuffer;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug, Default)]
+pub enum Difficulty {
+    Easy,
+    #[default]
+    Medium,
+    Hard,
+}
+
+impl fmt::Display for Difficulty {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Difficulty::Medium => write!(f, "medium"),
+            Difficulty::Hard => write!(f, "hard"),
+            &Difficulty::Easy => write!(f, "easy"),
+        }
+    }
+}
+
+//CLI
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    /// Optional name to operate on
+
+    #[arg(long)]
+    file_path: Option<String>,
+    #[arg(long, default_value_t = 120)]
+    ball_speed: usize,
+    #[arg(long, default_value_t = Difficulty::Medium)]
+    difficulty: Difficulty,
+    #[arg(long, default_value_t = false)]
+    handicap: bool,
+}
+//CLI END
+
+//COLOURS MANAGEMENT
+pub fn rgb(red: u8, green: u8, blue: u8) -> u32 {
+    let a = u32::from(red);
+    let b: u32 = u32::from(green);
+    let c = u32::from(blue);
+
+    let new_red = a << 16;
+    let new_green = b << 8;
+
+    let final_number = new_red | new_green | c;
+
+    return final_number;
+}
+//COLOURS MANAGEMENT END
+
+pub struct World {
+    player_1_pong: Vec<(usize, usize)>,
+    player_2_pong: Vec<(usize, usize)>,
+    player_1_score: usize,
+    player_1_score: usize,
+    ball: (usize, usize),
+    finished: bool,
+    small_break_timer: Instant,
+    space_count: usize,
+    game_speed: usize,
+}
+
+impl World {
+    pub fn new(
+        player_1_pong: Vec<(usize, usize)>,
+        player_2_pong: Vec<(usize, usize)>,
+        player_1_score: usize,
+        player_1_score: usize,
+        ball: (usize, usize),
+        finished: bool,
+        small_break_timer: Instant,
+        space_count: usize,
+        game_speed: usize,
+    ) -> Self {
+        Self {
+            player_1_pong,
+            player_2_pong,
+            player_1_score,
+            player_1_score,
+            ball,
+            finished,
+            small_break_timer,
+            space_count,
+            game_speed,
+        }
+    }
+}
+
+pub fn creation_pongs (world: &mut World, buffer: WindowBuffer) {
+    let x_middle_point = buffer.width() / 2;
+    let y_middle_point = buffer.height() / 2;
+
+    world.player_1_pong.push((0, y_middle_point));
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use lib:: World;
+
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
+
+    let mut buffer: WindowBuffer = WindowBuffer::new(cli.width, cli.height);
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        buffer.width(),
+        buffer.height(),
+        WindowOptions {
+            scale: minifb::Scale::X16,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut game_elements: World = World::new(
+        Vec::new(),
+        Vec::new(),
+        0,
+        0,
+        (0, 0),
+        false,
+        Instant::now(),
+        0,
+        cli.game_speed, 
+    );
+
+    let mut instant = Instant::now();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let _ = game_elements.handle_user_input(&window, &cli, &buffer);
+
+
+
+        window
+            .update_with_buffer(&buffer.buffer(), cli.width, cli.height)
+            .unwrap();
+    }
+    Ok(())
 }
